6c70d6a7319781d8b2149ecbf0ac0ebed6cdb7ed
ali shirzadeh <ashirzade5@gmail.com>
2022-03-13 13:48:35 +0330

    create muliple cpmponents

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 
-// dynamic value 
+
 
 function App() {
-  const title = 'welcome to the new blog!'
-  const likes = 50
-  const link = "https://www.google.com"
-
-
   return (
     <div className="App">
-       <div className='content'>
-           <h1>
-            {title}
-           </h1>
-           <p>
-             Likes {likes} times
-           </p>
-           <p>
-            { [1,2,3,4,'aa']}
-           </p>
-           <p>
-             {10}
-           </p>
-           <p>
-             {Math.random() * 10}
-           </p>
-
-           <a href={link}>Google site</a>
-       </div>
+        <Navbar />
+        <div className='content'>
+          <Home />
+        </div>
     </div>
   );
 }
