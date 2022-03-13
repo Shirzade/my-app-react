6a2246dc6d6ce08f6ca0f11f1e38e8d725a45b22
ali shirzadeh <ashirzade5@gmail.com>
2022-03-13 13:34:17 +0330

    danamic values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// dynamic value 
+
 function App() {
+  const title = 'welcome to the new blog!'
+  const likes = 50
+  const link = "https://www.google.com"
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='content'>
+           <h1>
+            {title}
+           </h1>
+           <p>
+             Likes {likes} times
+           </p>
+           <p>
+            { [1,2,3,4,'aa']}
+           </p>
+           <p>
+             {10}
+           </p>
+           <p>
+             {Math.random() * 10}
+           </p>
+
+           <a href={link}>Google site</a>
+       </div>
     </div>
   );
 }
